1dba2b8abb0f31a01756d7db3730efa500ae02c1
Mederbek Burkanov <mederburkanov@gmail.com>
2022-10-05 17:49:58 +0600

    initial commit

---
+++ coder-swag/coder-swag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Mederbek on 5/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
